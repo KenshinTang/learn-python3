ae32b1102e473dd5c1627ded68f473acd1b6675d
KenshinTang <28135055@qq.com>
2017-10-11 23:11:26 +0800

    sub process 模块

---
+++ src/processAndThread/subprocessing.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+print('nslookup www.python.org')
+r = subprocess.call(['nslookup', 'www.python.org'])
+print('exit code: ', r, 'utf-8')
